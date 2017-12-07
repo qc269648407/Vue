ed6d0b8e506770a7f36a85917b50414fd663b443
QC <17798502614@189.cn>
2017-12-07 15:16:56 +0800

    vuex的学习

---
+++ VueX/vueX/README.md
@@ -0,0 +1,24 @@
+# vuex
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+
+疑惑参照http://blog.csdn.net/h5_queenstyle12/article/details/75386359
---
+++ VueX/vueX/src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+    <h3>{{count}}</h3>
+    <button @click="$store.commit('add',1)">增加1</button>
+    <button @click="$store.commit('reduce')">减少1</button>
+    <button @click="$store.commit('add',2)">增加2</button>
+  
+    <button @click="add(10)">增加1</button>
+  
+    <!-- <button @click="clickAdd()">增加</button>
+      <button @click="clickReduce()">减少</button> -->
+  </div>
+</template>
+
+<script>
+  //必须引入，调用数据使用
+  import store from '../store/store'
+  
+  //引入可以简写函数的调用
+  import {
+    mapMutations
+  } from 'vuex';
+  export default {
+  
+    name: 'HelloWorld',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    },
+    computed: {
+      count() {
+        return this.$store.state.count;
+      }
+    },
+    methods: {
+      clickAdd() {
+        store.state.count++;
+      },
+      clickReduce() {
+        store.state.count--;
+      }
+    },
+    methods: mapMutations([
+      'add', 'reduce'
+    ]),
+    store
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  
+</style>
